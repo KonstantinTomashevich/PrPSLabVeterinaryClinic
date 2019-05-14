e7b9887376f9c90294e42e5e1347a0613d40f9e9
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-05-14 17:17:43 +0300

    Implement PatientListViewForm.

--- src/crud_patient/PatientListViewForm.java
+++ src/crud_patient/PatientListViewForm.java
@@ -1,27 +1,68 @@
 package crud_patient;
 
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
+import javafx.util.Pair;
 
-@objid ("f29d7042-afc2-42e3-9f13-5d956e4cdd1a")
+import javax.swing.*;
+import java.util.List;
+
+@objid("f29d7042-afc2-42e3-9f13-5d956e4cdd1a")
 public class PatientListViewForm implements RecordListViewForm {
-    @objid ("535c5382-f8af-415f-8a75-a70b6e581c95")
+    private JPanel mainPanel;
+    private JList patientsList_;
+    private JButton viewSelectedPatientButton;
+    private JFrame frame_;
+    private List<Pair<Integer, String>> currentPatientsList_ = null;
+    private int selectedClient = -1;
+
+    public PatientListViewForm() {
+        super();
+        viewSelectedPatientButton.addActionListener(e -> ViewSelectedRecord());
+    }
+
+    @objid("535c5382-f8af-415f-8a75-a70b6e581c95")
     public void ShowOnlyClientPatients(final int clientId) {
+        selectedClient = clientId;
+        Show();
     }
 
-    @objid ("dbb3e46d-a7b3-459b-9c47-cc866e66dc35")
+    @objid("dbb3e46d-a7b3-459b-9c47-cc866e66dc35")
     public void Show() {
+        frame_ = new JFrame("Patient list" + (selectedClient != -1 ? "for client " + selectedClient : ""));
+        FetchData();
+        frame_.add(mainPanel);
+        frame_.pack();
+        frame_.setVisible(true);
+        frame_.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
-    @objid ("d7c1f888-2707-4a2c-8928-ca6e9580dbb1")
+    @objid("d7c1f888-2707-4a2c-8928-ca6e9580dbb1")
     public void FetchData() {
+        DefaultListModel<String> model = new DefaultListModel<>();
+        currentPatientsList_ = selectedClient == -1 ?
+                PatientDatabaseAcessor.instance.GetAllPatientsIDs() :
+                PatientDatabaseAcessor.instance.GetAllClientPatientsIDs(selectedClient);
+
+        if (currentPatientsList_ != null) {
+            for (Pair<Integer, String> data : currentPatientsList_) {
+                model.addElement(data.getKey() + ": " + data.getValue());
+            }
+        }
+
+        patientsList_.setModel(model);
     }
 
-    @objid ("4abb0da3-b531-488f-9953-7528a1a6a663")
+    @objid("4abb0da3-b531-488f-9953-7528a1a6a663")
     public void ViewSelectedRecord() {
+        int index = patientsList_.getSelectedIndex();
+        if (index >= 0 && index < currentPatientsList_.size()) {
+            new PatientRecordViewForm().ShowFor(currentPatientsList_.get(index).getKey());
+        }
     }
 
-    @objid ("5022babe-bff1-4376-a2f6-12f1f45568da")
+    @objid("5022babe-bff1-4376-a2f6-12f1f45568da")
     public void Dispose() {
+        frame_.setVisible(false);
     }
 
 }
