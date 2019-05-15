3befda73ece5a311f3f3b824daa38424fd46f8c7
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-05-15 16:15:46 +0300

    VeterinaryCard: add id field.

--- src/crud_patient/VeterinaryCard.java
+++ src/crud_patient/VeterinaryCard.java
@@ -8,6 +8,7 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 @DataType
 @objid ("97e9407c-ba40-46a6-b418-943949a41ef2")
 public class VeterinaryCard {
+    public int id;
     @objid ("510e15cc-cd39-4ef6-ae3b-1816cb048630")
     public List<Appointment> appointments = new ArrayList<Appointment> ();
 }
