718acfdb305fe7a641d17df3a48a1352c731402c
Konstantin Tomashevich <ekit.mail@gmail.com>
2019-05-16 23:54:44 +0300

    Implement DatePeriodSelectionForm.

--- src/reports/DatePeriodSelectorForm.java
+++ src/reports/DatePeriodSelectorForm.java
@@ -1,15 +1,46 @@
 package reports;
 
+import check.CheckService;
 import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
-@objid ("392acd43-6331-4405-ae82-97ea4b8a3eb0")
+import javax.swing.*;
+import java.text.ParseException;
+import java.util.Date;
+import java.util.function.BiConsumer;
+
+@objid("392acd43-6331-4405-ae82-97ea4b8a3eb0")
 public class DatePeriodSelectorForm {
-    @objid ("f861f7cd-b3b1-477e-8c57-0f6914c36c49")
-    public void Show() {
+    private JButton selectButton;
+    private JTextField startDateTextField;
+    private JTextField endDateTextField;
+    private JPanel mainPanel;
+    private JFrame frame_;
+
+    @objid("f861f7cd-b3b1-477e-8c57-0f6914c36c49")
+    public void Show(BiConsumer<Date, Date> callback) {
+        frame_ = new JFrame();
+        selectButton.addActionListener(e -> SelectClicked(callback));
+        frame_.add(mainPanel);
+        frame_.pack();
+        frame_.setVisible(true);
+        frame_.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
     }
 
-    @objid ("0cbfd093-9989-4031-b83a-085d90c1a958")
+    private void SelectClicked(BiConsumer<Date, Date> callback) {
+        try {
+            Date start = CheckService.dataFormat.parse(startDateTextField.getText());
+            Date end = CheckService.dataFormat.parse(endDateTextField.getText());
+            callback.accept(start, end);
+            frame_.setVisible(false);
+        } catch (ParseException exception) {
+            JOptionPane.showMessageDialog(frame_, "Unable to parse dates!",
+                    "Error!", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    @objid("0cbfd093-9989-4031-b83a-085d90c1a958")
     public void Hide() {
+        frame_.setVisible(false);
     }
 
 }
